2a6fef09b1a7e738b02bb9c05254fc26f8db314e
Sung Ahn Kim <ccoroom@gmail.com>
2014-11-16 23:23:07 +0900

    Remove 'Pods' folder from version control.

--- Example/Pods/Headers/Public/CPAutoLayout/View+CPAutoLayout.h
+++
@@ -1,1 +0,0 @@
-../../../../../Pod/Classes/View+CPAutoLayout.h
--- Example/Pods/Target Support Files/Pods-Tests-CPAutoLayout/Pods-Tests-CPAutoLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_CPAutoLayout : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_CPAutoLayout
-@end
