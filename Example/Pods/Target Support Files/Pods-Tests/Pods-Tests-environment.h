0189b9d7b2a36514186602578fb403c537abb098
Sung Ahn Kim <ccoroom@gmail.com>
2015-07-31 22:25:53 +0900

    Remove unused libraries.

--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -12,33 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_CPAutoLayout 8
 #define COCOAPODS_VERSION_PATCH_CPAutoLayout 0
 
-// Expecta
-#define COCOAPODS_POD_AVAILABLE_Expecta
-#define COCOAPODS_VERSION_MAJOR_Expecta 1
-#define COCOAPODS_VERSION_MINOR_Expecta 0
-#define COCOAPODS_VERSION_PATCH_Expecta 2
-
-// Expecta+Snapshots
-#define COCOAPODS_POD_AVAILABLE_Expecta_Snapshots
-#define COCOAPODS_VERSION_MAJOR_Expecta_Snapshots 1
-#define COCOAPODS_VERSION_MINOR_Expecta_Snapshots 3
-#define COCOAPODS_VERSION_PATCH_Expecta_Snapshots 4
-
-// FBSnapshotTestCase
-#define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
-#define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
-#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 8
-#define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 1
-
 // Masonry
 #define COCOAPODS_POD_AVAILABLE_Masonry
 #define COCOAPODS_VERSION_MAJOR_Masonry 0
 #define COCOAPODS_VERSION_MINOR_Masonry 6
 #define COCOAPODS_VERSION_PATCH_Masonry 2
-
-// Specta
-#define COCOAPODS_POD_AVAILABLE_Specta
-#define COCOAPODS_VERSION_MAJOR_Specta 0
-#define COCOAPODS_VERSION_MINOR_Specta 2
-#define COCOAPODS_VERSION_PATCH_Specta 1
 
